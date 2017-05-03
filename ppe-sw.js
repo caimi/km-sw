1af3299ef591b0dadbf1da67c5d3a20e6bfae868
carlos caimi <mkt@caimi.com.br>
2017-05-03 09:19:16 -0300

    metodos fetchs

--- ppe-sw.js
+++ ppe-sw.js
@@ -1,5 +1,6 @@
 var CACHE_NAME = 'ppe-sw-app';
 var urlsToCache = [
+  './',
   './index.html',
   './css/ftsl.css',
   './css/materialize.css',
@@ -18,3 +19,29 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request);
+  }));
+});
+
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+     fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+
+    }, reject);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
